317416e9105af7bd2ca6faa8c9a80582418c740f
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2024-11-19 17:36:48 -0500

    added basic code for the navbar
    added the barebones navbar

---
+++ Frontend/src/Navbar.jsx
@@ -0,0 +1,23 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <span>
+          <a href="#">BizPoints</a>
+        </span>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Dashboard</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
