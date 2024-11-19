4af9c3f2ed7e8a31699c4717ab545ca005ccb124
Zaid Khan <59452329+ZaidKhan02@users.noreply.github.com>
2024-11-19 16:58:05 -0500

    modified dashboard files

--- Frontend/src/Dashboard.jsx
+++ Frontend/src/Dashboard.jsx
@@ -3,8 +3,17 @@ import "./Dashboard.css"
 
 export default function Dashboard({ }) {
     return (
-        <div>
+        <div className="dashboard">
             <h1>Account Dashboard</h1>
+            <div className="card">
+                <h2>Point Balance</h2>
+            </div>
+            <div className="card">
+                <h2>Transaction History</h2>
+            </div>
+            <div className="card">
+                <h2>Activity Logs</h2>
+            </div>
         </div>
     )
 } 
